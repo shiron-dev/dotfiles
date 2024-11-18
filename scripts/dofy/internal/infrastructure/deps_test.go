ff57c79ff0ea03e293e747ef375d1707af62b953
shiron <shiron4710.dev@gmail.com>
2024-11-18 10:58:43 +0900

    test: add check is installed to deps test

--- scripts/dofy/internal/infrastructure/deps_test.go
+++ scripts/dofy/internal/infrastructure/deps_test.go
@@ -2,6 +2,7 @@ package infrastructure_test
 
 import (
 	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/shiron-dev/dotfiles/scripts/dofy/internal/di"
@@ -17,15 +18,29 @@ func TestCheckInstalled(t *testing.T) {
 
 	deps := infra.DepsInfrastructure
 
-	installed := deps.CheckInstalled("git")
+	const notInstalledCommand = "not_installed_command"
 
-	if !installed {
-		t.Fatal("git is not installed")
+	_, err = exec.LookPath(notInstalledCommand)
+	if err == nil {
+		t.Fatalf("expected error, got nil")
 	}
 
-	installed = deps.CheckInstalled("not_installed")
+	installed := deps.CheckInstalled(notInstalledCommand)
 
 	if installed {
 		t.Fatal("not_installed is installed")
+	}
+
+	const installedCommand = "git"
+
+	_, err = exec.LookPath(installedCommand)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	installed = deps.CheckInstalled(installedCommand)
+
+	if !installed {
+		t.Fatal("git is not installed")
 	}
 }
