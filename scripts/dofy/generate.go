51454b7f15b0eb77eb6b700ff99aa40955efa03f
shiron <shiron4710.dev@gmail.com>
2024-11-21 22:49:36 +0900

    chore: add gotests templates

--- scripts/dofy/generate.go
+++ scripts/dofy/generate.go
@@ -1,4 +1,4 @@
 package main
 
 //go:generate go run github.com/google/wire/cmd/wire gen internal/di/wire.go
-//go:generate go run github.com/cweill/gotests/gotests -all -exported -parallel -w internal/infrastructure/brew.go
+//go:generate go run github.com/cweill/gotests/gotests -all -exported -parallel -w -template_dir internal/test/templates internal/infrastructure/brew.go
