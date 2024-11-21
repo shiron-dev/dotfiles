912724188b71fe4e9f027b9abe27832bbe82e253
shiron <shiron4710.dev@gmail.com>
2024-11-21 22:00:45 +0900

    a

--- scripts/dofy/generate.go
+++ scripts/dofy/generate.go
@@ -1,3 +1,9 @@
 package main
 
 //go:generate go run github.com/google/wire/cmd/wire gen internal/di/wire.go
+//go:generate go run go.uber.org/mock/mockgen -source=internal/infrastructure/brew.go -destination=./gen/mock/infrastructure/brew.go
+//go:generate go run go.uber.org/mock/mockgen -source=internal/infrastructure/config.go -destination=./gen/mock/infrastructure/config.go
+//go:generate go run go.uber.org/mock/mockgen -source=internal/infrastructure/deps.go -destination=./gen/mock/infrastructure/deps.go
+//go:generate go run go.uber.org/mock/mockgen -source=internal/infrastructure/file.go -destination=./gen/mock/infrastructure/file.go
+//go:generate go run go.uber.org/mock/mockgen -source=internal/infrastructure/git.go -destination=./gen/mock/infrastructure/git.go
+//go:generate go run go.uber.org/mock/mockgen -source=internal/infrastructure/printout.go -destination=./gen/mock/infrastructure/printout.go
