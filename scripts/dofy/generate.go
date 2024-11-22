f4d88ab6a4fea105e0c0a53f637658964876932b
shiron <shiron4710.dev@gmail.com>
2024-11-22 23:20:43 +0900

    feat: update test to gotests format

--- scripts/dofy/generate.go
+++ scripts/dofy/generate.go
@@ -1,4 +1,9 @@
 package main
 
 //go:generate go run github.com/google/wire/cmd/wire gen internal/di/wire.go
-//go:generate go run github.com/cweill/gotests/gotests -all -exported -parallel -w -template_dir internal/test/templates internal/infrastructure/brew.go
+//go:generate go run github.com/cweill/gotests/gotests -all -exported -parallel -w -excl New.+ -template_dir internal/test/templates internal/infrastructure/brew.go
+//go:generate go run github.com/cweill/gotests/gotests -all -exported -parallel -w -excl New.+ -template_dir internal/test/templates internal/infrastructure/config.go
+//go:generate go run github.com/cweill/gotests/gotests -all -exported -parallel -w -excl New.+ -template_dir internal/test/templates internal/infrastructure/deps.go
+//go:generate go run github.com/cweill/gotests/gotests -all -exported -parallel -w -excl New.+ -template_dir internal/test/templates internal/infrastructure/file.go
+//go:generate go run github.com/cweill/gotests/gotests -all -exported -parallel -w -excl New.+ -template_dir internal/test/templates internal/infrastructure/git.go
+//go:generate go run github.com/cweill/gotests/gotests -all -exported -parallel -w -excl New.+ -template_dir internal/test/templates internal/infrastructure/printout.go
