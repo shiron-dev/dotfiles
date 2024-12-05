b46aec70b27a0e20373de87f23b0a270ec2f5e74
shiron <shiron4710.dev@gmail.com>
2024-12-05 11:31:26 +0900

    feat(zsh): sep function

--- config/zsh/.config/tools.zsh
+++ config/zsh/.config/tools.zsh
@@ -70,29 +70,5 @@ alias dofy="/Users/shiron/projects/dotfiles/scripts/dofy/dofy"
 alias docker-compose-rm="docker compose down --rmi all --volumes --remove-orphans"
 alias lsusb="system_profiler SPUSBDataType"
 
-# My Functions
-_notify() {
-  afplay /System/Library/Sounds/Hero.aiff
-  osascript -e 'display notification with title "Terminal"'
-}
-alias notify=_notify
-
-function moveToTrash() {
-  local p
-  for p in "$@"; do
-    if [[ "$p" == -* ]]; then
-      continue
-    fi
-
-    if [ -e "$p" ]; then
-      date=$(/usr/bin/env date "+%Y-%m-%d_%H-%M-%S")
-      /usr/bin/env mkdir -p ~/.Trash/$p_$date/..
-      /usr/bin/env mv "$p" ~/.Trash/$p_$date
-    else
-      /usr/bin/env echo "Error: '$p' does not exist."
-    fi
-  done
-}
-alias rm='moveToTrash'
-
-function gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$@; }
+# My functions
+source ~/.config/zsh/functions.zsh
