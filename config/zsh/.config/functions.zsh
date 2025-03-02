31e822b225624fe80639f408e6b36fe076d918e3
Itsuki Shirahige <shiron4710.dev@gmail.com>
2025-03-03 00:37:10 +0900

    style: lint
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- config/zsh/.config/functions.zsh
+++ config/zsh/.config/functions.zsh
@@ -26,7 +26,7 @@ alias rm='moveToTrash'
 
 function gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$@; }
 
-function gitbc() { git checkout -q main && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done }
+function gitbc() { git checkout -q main && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done; }
 
 alias gc="ghq get"
 
